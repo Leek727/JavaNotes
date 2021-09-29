7cc42ceaedee15b53ea30ca916506cc235ebd0b6
Leek727 <91506829+Leek727@users.noreply.github.com>
2021-09-29 02:16:54 +0000

    start of OnelineFormatter.java - program that turns other java programs into one line to annoy people

---
+++ OnelineFormatter.java
@@ -0,0 +1,10 @@
+class OnelineFormatter {
+  public static void main(String[] args){
+    System.out.println();
+  }
+}
+
+/* Compile - no args yet
+javac OnelineFormatter.java 
+java OnelineFormatter
+*/
